dc71f9523b26b5b0f713e6f18616c2f33b006c2c
Henry Bond <henrybond158@me.com>
2013-04-25 10:13:52 +0100

    1st commit

---
+++ PortScannerTest/PortScanningClass.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+namespace PortScannerTest
+{
+    class PortScanningClass
+    {
+
+        // IsIpAddress
+        //
+        // The following routine returns true if a given string is a valid IP address 
+
+        static bool IsIpAddress(string Address)
+        {
+            // The following pattern matches an IP address 
+            Regex IpMatch = new Regex(@"\b(?:\d{1,3}\.){3}\d{1,3}\b");
+            return IpMatch.IsMatch(Address);
+        }
+
+        // LookupDNSName
+        //
+        // 
+
+        static bool LookupDNSName(string ScanAddress, out IPAddress ScanIPAddress)
+        {
+            ScanIPAddress = null;
+            IPHostEntry NameToIpAddress;
+
+            try
+            {
+                // Lookup the address we are going to scan 
+                NameToIpAddress = Dns.GetHostEntry(ScanAddress);
+            }
+            catch (SocketException)
+            {
+                // Thrown when we are unable to lookup the name
+                return false;
+            }
+
+            // Pick the first address in the list , there should be at least 1 
+            if (NameToIpAddress.AddressList.Length > 0)
+            {
+                ScanIPAddress = NameToIpAddress.AddressList[0];
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool ScanPort(IPAddress Address, int Port)
+        {
+            TcpClient Client = new TcpClient();
+            try
+            {
+                // Attempt to connect to the given address + port 
+                Client.Connect(Address, Port);
+
+                // This may seem like an avoidable step -- but TcpClient.Close does not
+                // actually close the underlying connection
+                // http://support.microsoft.com/default.aspx?scid=kb%3Ben-us%3B821625
+
+                NetworkStream ClientStream = Client.GetStream();
+                ClientStream.Close();
+
+                // Free the TCPClient resource
+                Client.Close();
+            }
+            catch (SocketException)
+            {
+                // Assume that a socket exception means the connection failed
+                // Client.Connect returns a void (so provides no insights into 
+                // what it was doing)
+                return false;
+            }
+            return true;
+        }
+
+        public void portScan(string[] args)
+        {
+            int[] popenPortArray = new int[100];
+            int counter = 0;
+
+            String ScanAddress;
+            IPAddress ScanIPAddress;
+
+            try
+            {
+                // Try to read the scan address from the command line, or default to localhost
+                if (args.Length != 0)
+                    ScanAddress = args[0];
+                else
+                    ScanAddress = "127.0.0.1";
+
+                // Both a hostname or an IP address are fine
+                if (IsIpAddress(ScanAddress))
+                {
+                    ScanIPAddress = IPAddress.Parse(ScanAddress);
+                }
+                else
+                    if (!LookupDNSName(ScanAddress, out ScanIPAddress))
+                    {
+                        Console.WriteLine("Error looking up {0}", ScanAddress);
+                        return;
+                    }
+                Console.WriteLine("working");
+                // Report what we are going to do
+                // Console.WriteLine("Port scanning {0} ({1})", ScanAddress, ScanIPAddress.ToString());
+
+                // Scan all the possible posts 
+                for (int Port = IPEndPoint.MinPort; Port < (IPEndPoint.MaxPort - 65000); Port++)
+                {
+                    // Console.Write("Scanning port {0} : ", Port);
+                    if (ScanPort(ScanIPAddress, Port))
+                    {
+                        popenPortArray[counter] = Port;
+
+                        Console.WriteLine("OPEN");
+                    }
+                    else { }
+                }
+                // Close Up
+
+                Console.WriteLine("open ports are:");
+                foreach (int i in popenPortArray)
+                {
+                    Console.WriteLine(i);
+
+                }
+                Console.WriteLine("Finished scanning all ports");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception caught!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+            }
+
+        }
+    }
+    }
+
---
+++ PortScannerTest/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace PortScanner
+{
+    class Program
+    {
+
+   
+        static void Main(string[] args)
+        {
+            int input = 0;
+          while (true) {
+            
+
+
+            Console.WriteLine("Welcome to the temporary menu");
+            Console.WriteLine("Select an option");
+            Console.WriteLine("1.: Port scan localhost");
+            Console.WriteLine("2: Twitter Search for #tag ");
+            Console.WriteLine("3. Find external IP");
+            Console.WriteLine("4: Hide the console window");
+            
+           // input = int.Parse(Console.ReadLine());
+            input = 0;
+
+            switch (input) {
+                case 1:
+                    PortScannerTest.PortScanningClass portScanner = new PortScannerTest.PortScanningClass();
+                    portScanner.portScan(args);
+                //portScan(args);
+          
+            break;
+
+                case 2:
+
+            Console.WriteLine("please input a search string\n");
+            string searchString;
+            searchString = Console.ReadLine();
+                    StringBuilder sb = new StringBuilder();
+                        byte[] buf = new byte[8192];
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://search.twitter.com/search.json?q=%23" + searchString);
+                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                        Stream readStream = response.GetResponseStream();
+                        StreamReader streamReader = new StreamReader(readStream, Encoding.UTF8);
+                        string responseFromServer = streamReader.ReadToEnd();
+
+                        Console.WriteLine(responseFromServer);
+                        string[] resonponses = Regex.Split(responseFromServer, "\"text\":");
+                        Console.WriteLine();
+                        foreach (string str in resonponses)
+                        {
+                            Console.WriteLine(str);
+
+
+                        }
+
+                        if (resonponses.Length != 1)
+                        {
+
+                            Console.WriteLine("Tweet found, triggering the port scan");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("no tweet present, falling back");
+
+                        }
+                        streamReader.Close();
+                        response.Close();
+                        Console.ReadLine();
+                    break;
+                case 3:
+                        HTTPGet req = new HTTPGet();
+                        req.Request("http://checkip.dyndns.org");
+                        string[] a = req.ResponseBody.Split(':');
+                        string a2 = a[1].Substring(1);
+                        string[] a3=a2.Split('<');
+                        string a4 = a3[0];
+                        Console.WriteLine(a4);
+                        Console.ReadLine();
+                    break;
+
+
+                case 4:
+
+                    break;
+                default:
+
+                    Console.WriteLine(" you choose a non option"); 
+            break;
+        }
+        }
+    }
+    }
+}
